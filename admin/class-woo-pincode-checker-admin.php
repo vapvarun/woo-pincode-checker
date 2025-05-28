d191f39130d1e9c69de2367e42effc1c90974061
Nitin Patil <pallavi@wbcomdesigns.com>
2025-05-28 15:59:12 +0530

    Remove unwanted files and code of wbcom folder
    https://3.basecamp.com/5798509/buckets/42374795/card_tables/cards/8689938857

--- admin/class-woo-pincode-checker-admin.php
+++ admin/class-woo-pincode-checker-admin.php
@@ -182,9 +182,6 @@ class Woo_Pincode_Checker_Admin {
 		<div class="wrap">
 			<div class="wbcom-bb-plugins-offer-wrapper">
 				<div id="wb_admin_logo">
-					<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/" target="_blank">
-						<img src="<?php echo esc_url( WPCP_PLUGIN_URL ) . 'admin/wbcom/assets/imgs/wbcom-offer-notice.png'; ?>">
-					</a>
 				</div>
 			</div>
 			<div class="wbcom-wrap">
